10528d5f6cd7c985b1094991aa041d8bfe18e992
shengliangd <shengliangd@outlook.com>
2022-09-30 20:37:14 +0800

    add robot name in nav2

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,1 @@
-if [ $# -eq 0 ]; then
-    echo "usage: $0 <interface>"
-    exit
-fi
-INTERFACE=$1
-CYCLONEDDS_URI="<CycloneDDS><Domain><General><NetworkInterfaceAddress>$INTERFACE</NetworkInterfaceAddress></General></Domain></CycloneDDS>"
-
-docker run -td --name ros2_wheeltec_robot -e DISPLAY=$DISPLAY -e CYCLONEDDS_URI=$CYCLONEDDS_URI -v /tmp/.X11-unix --network host --ipc host --privileged -v /dev:/dev ros2_wheeltec_robot bash
+docker run -td --name ros2_wheeltec_robot -e DISPLAY=$DISPLAY -v /tmp/.X11-unix --network host --ipc host --privileged -v /dev:/dev ros2_wheeltec_robot bash
