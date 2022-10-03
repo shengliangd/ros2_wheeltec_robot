8cd205f89d6109e1f42d6c9ee65157c57344241a
shengliangd <shengliangd@outlook.com>
2022-10-03 15:03:43 +0800

    fix robot info

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,4 @@ fi
 INTERFACE=$1
 CYCLONEDDS_URI="<CycloneDDS><Domain><General><NetworkInterfaceAddress>$INTERFACE</NetworkInterfaceAddress></General></Domain></CycloneDDS>"
 
-docker run -td --name ros2_wheeltec_robot -e DISPLAY=$DISPLAY -e CYCLONEDDS_URI=$CYCLONEDDS_URI -v /tmp/.X11-unix --network host --ipc host --privileged -v /dev:/dev -v `readlink -f ./wheeltec-ros2-src/src`:/work/ros2_ws/src ros2_wheeltec_robot bash
+docker run -td --name ros2_wheeltec_robot -e DISPLAY=$DISPLAY -e CYCLONEDDS_URI=$CYCLONEDDS_URI -v /tmp/.X11-unix --network host --ipc host --privileged --shm-size 8G -v /dev:/dev -v `readlink -f ./wheeltec-ros2-src/src`:/work/ros2_ws/src ros2_wheeltec_robot bash
